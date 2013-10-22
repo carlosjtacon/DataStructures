93c43b92d8762a9fc0a688b6c157b4ec30bb9d09
Carlos Tacón <carlosjtacon@gmail.com>
2013-10-22 15:23:00 +0200

    Functions coded
    I've code some functions (isEmpty, makenull, front), enqueue and
    dequeue are still null

--- practice1/queue.cpp
+++ practice1/queue.cpp
@@ -13,36 +13,44 @@ struct node
 class Queue
 {
 private:
-	node* front;
+	node* _front;
 
 public:
 	Queue()
 	{
 		/*constructor*/
+		_front == NULL;
 	}
 	~Queue()
 	{
 		/*destructor*/
 	}
 
-	void enqueue()
+	void enqueue(char c)
 	{
 		//Insert an element at the rear of the queue
 	}
+
 	char dequeue()
 	{
 		//Delete the element at the front of queue (and return it)
 	}
+
 	char front()
 	{
 		//Return the element at the front of the queue
+		return _front -> data;
 	}
+
 	void makenull()
 	{
 		//Make the queue to be an empty queue
+		_front =NULL;
 	}
+
 	bool isEmpty()
 	{
 		//Return true if the queue is empty, return false otherwise
+		return (_front == NULL);
 	}
 };
