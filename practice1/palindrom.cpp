514cc3b4765fe14d012b2fcff64e8617970e723b
mario <mario@MBR.(none)>
2013-10-25 17:30:15 +0200

    part of the code has been commented to avoid compilation errors, since queue.cpp hasnt' been finished e.e

--- practice1/palindrom.cpp
+++ practice1/palindrom.cpp
@@ -1,22 +1,22 @@
 #include "palindrom.h"
 
-int isPalindrom(string input){
+int isPalindrom(std::string input){
 	//get string into stack
 	Stack stack;
-	for(int i=0, i<input.size(), i++){
+	for(int i=0; i<input.size(); i++){
 		stack.push(input[i]);
-	}
+	}/*
 	//get string into queue
 	Queue queue;
-	for(int i=0, i<input.size(), i++){
+	for(int i=0; i<input.size(); i++){
 		queue.enqueue(input[i]);	
 	}	
 	
 	//compare both structures
-	for(int i=0, i<input.size(), i++){
+	for(int i=0; i<input.size(); i++){
 		if(stack.pop() != queue.dequeue){
 			return 0;
 		}
-	} 
+	} */
 	return 1;
 }
