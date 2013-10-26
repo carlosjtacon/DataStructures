59026a4a12080eeada173be7623acb296b067a58
mario <mario@MBR.(none)>
2013-10-26 19:29:38 +0200

    improved output messages and fixed a problem with the 'input' string

--- practice1/main.cpp
+++ practice1/main.cpp
@@ -11,34 +11,35 @@ using namespace std;
 
 int main(int argc, const char* argv[]){
 	
-	string input=0;
+	string input ("\0");
 	
-	if (argc > 2){
-		cout << "there's way too much arguments :)";
+	if (argc > 3){
+		cout << "there's way too much arguments :) \n";
 		return 0;
-	}else if(argc ==1){ //just the word to be checked if palindrom or not
-		input = argv[0];
-	}else if(argc == 2){//-f modifier & name of file to be checked
-		if(strcmp(argv[0],"-f")==0){
-			cout << "paremeter unknown " + *argv[0];
+	}else if(argc ==2){ //just the word to be checked if palindrom or not
+		input = argv[1];
+	}else if(argc == 3){//-f modifier & name of file to be checked
+		if(strcmp(argv[1],"-f")==0){
+			cout << "paremeter unknown " + *argv[1];
+			cout <<  " \n";
 		}else{// if everything is ok
-			ifstream inputFile (argv[1]);
+			ifstream inputFile (argv[2]);
 			if(inputFile.is_open()){
 				string tmp;
 				while(getline(inputFile, tmp))
 					input+=tmp;
 			}//now we've got the file text in 'input' string
 			else{
-				cout << "the file couldn't be opened, maybe doesn't exists";
+				cout << "the file couldn't be opened, maybe doesn't exists \n";
 				return 0;
 			}
 		}
 	}
 
 	if(isPalindrom(input)){
-		cout << input + "is palindrom";
+		cout << input + " is palindrom \n";
 	}else{
-		cout << input + "is not palindrom";
+		cout << input + " is not palindrom \n";
 	}
 	return 0;
 }
