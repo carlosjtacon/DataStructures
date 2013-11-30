e3f8941359e0248c8a3bc53a3fe5fc1f19a654da
Carlos Tacón <carlosjtacon@gmail.com>
2013-11-30 17:18:44 +0100

    it compliles but exec error found

--- practice2/main.cpp
+++ practice2/main.cpp
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 #include <fstream>
 #include <string.h>
+#include <vector>
 #include "queue.h"
 #include "stack.h"
 #include "functions.h"
 
 using namespace std;
 
-int main(int argc, const char* argv[])
+int main()
 {
 	
-	#define HELP "Usage: ./palindrome [parameter]\nWhere: parameter is one of:\n\t\t-h (help)\n\t\t-k (read from keyboard)\n\t\t-f file (read from file)\n\t\t-d letter (remove all palindromes which start by this letter)\n\t\t-s (show data)\n\t\t-q (exit)\n";
+	#define HELP "Usage: $ palindrome [parameter]\nWhere: parameter is one of:\n\t\t-h (help)\n\t\t-k (read from keyboard)\n\t\t-f file (read from file)\n\t\t-d letter (remove all palindromes which start by this letter)\n\t\t-s (show data)\n\t\t-q (exit)\n";
 	
 	string input ("\0");
 	string original ("\0");
@@ -25,98 +26,93 @@ int main(int argc, const char* argv[])
 	{
 		//print a new prompt until -q
 		cout << "$ palindrome ";
+
+		//read command
 		getline(cin, command, '\n');
+
+		//create our own argc
 		int myArgc = numWords(command);
-		queue<string> myArgv;
-		for (int i = 0; i < command.length(); ++i)
+
+		//create our own argv
+		vector<string> myArgv;
+		istringstream iss(command);
+		string token;
+		while(getline(iss, token, ' '))
 		{
-			//add word to array
+			myArgv.push_back(token);
+		} 
+
+		//main method
+		if (myArgc == 1)
+		{
+			if (myArgv.at(1) == "-h")
+			{
+				cout << HELP;
+			}
+			else if (myArgv.at(1) == "-s")
+			{
+				//show in the screen the number of queues in the list, the total number of palindromes in each queue and their starting letter
+			}
+			else if (myArgv.at(1) == "-k")
+			{
+				int i = 0;
+
+				while(original!="q")
+				{
+					cout << "(" << i << ") "; i++;
+					getline(cin, original, '\n');
+					input = rmSpace(original);
+
+					if (isPalindrome(input) && original!="q")
+					{
+						//add original to list
+					}
+				} 
+			}
+			else
+			{
+				cout << "Paremeter unknown " << myArgv.at(1) << endl;
+				cout << HELP;
+			}
+		}
+		else if (myArgc == 2)
+		{
+			if (myArgv.at(1) == "-f")
+			{
+				ifstream inputFile (myArgv.at(2));
+				if(inputFile.is_open())
+				{
+					string tmp;
+					while(getline(inputFile, tmp))
+					{
+						if (isPalindrome(tmp))
+						{
+							//add tmp to list
+						}
+					}
+				}	
+				else
+				{
+					//filename does not exists
+					cout << "The file couldn't be opened, maybe doesn't exists :(\n";
+					cout << HELP;
+				}
+			}
+			else if (myArgv.at(1) == "-d")
+			{
+				//Remove all palindromes which start by a specific letter
+			}
+			else
+			{
+				cout << "Paremeter unknown " << myArgv.at(1) << endl;
+				cout << HELP;
+			}
+		}
+		else if (myArgc > 2)
+		{
+			cout << "There's way too much arguments :')\n";
+			cout << HELP;
 		}
 	}
 	while(command != "-q");
-/*
-	if (argc == 1)
-	{
-		//print the help menu
-		cout << HELP;
-		return 0;
-	}
-	else if(argc == 2)
-	{
-		if (strcmp(argv[1],"-f")==0)
-		{
-			//command -f without passing file
-			cout << "Which file do you want to open? Pass it in arguments :)\n";
-			cout << HELP;
-			return 0;
-		}
-		else if (strcmp(argv[1],"-k")==0)
-		{
-			//read strings from keyboard
-			int i = 0;
-
-			while(original!="q")
-			{
-				cout << "(" << i << ") "; i++;
-				getline(cin, original, '\n');
-				input = rmSpace(original);
-
-				if (isPalindrome(input) && original!="q")
-				{
-					//list.add(original);
-				}
-			} 
-		}
-		else
-		{
-			// if wrong parameter
-			cout << "Paremeter unknown " << argv[1] << endl;
-			cout << HELP;
-			return 0;
-		}
-	}
-	else if(argc == 3)
-	{
-		if(strcmp(argv[1],"-f")==0)
-		{	
-			// if everything is ok
-			ifstream inputFile (argv[2]);
-			if(inputFile.is_open())
-			{
-				string tmp;
-				while(getline(inputFile, tmp))
-				{
-					//INSERTAR
-				}
-			}	
-			else
-			{
-				//filename does not exists
-				cout << "The file couldn't be opened, maybe doesn't exists :(\n";
-				cout << HELP;
-				return 0;
-			}
-		}
-		else if (strcmp(argv[1],"-k")==0)
-		{
-			//using command -k with more arguments
-			cout << "This parameter is used without arguments, try again!\n";
-			cout << HELP;
-			return 0;
-		}
-		else
-		{	
-			// if wrong parameter
-			cout << "Paremeter unknown " << argv[1] << endl;
-			cout << HELP;
-			return 0;
-		}
-	}
-	else if (argc > 3)
-	{
-		//passing more than 2 arguments (2+ ./palindrome = 3)
-		cout << "There's way too much arguments :)\n";
-		cout << HELP;
-		return 0;
-	}*/
 }
