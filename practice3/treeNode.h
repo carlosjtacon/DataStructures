6d4cb6a22dd5cf48960e5ee112339fcb733ae73c
Mario <mario@Marios-Mac-Pro.local>
2013-12-28 20:34:28 +0100

    new node for tree
    contains a string label (data) and a boolean (int) about it’s
    palindrome condition. Furthermore it contains the leftchild &
    rightchild pointers and thee balance factor (AVL need)

---
+++ practice3/treeNode.h
@@ -0,0 +1,13 @@
+#ifndef TREE_NODE
+#define TREE_NODE
+
+struct treeNode
+{
+	string label;
+	int palindrom;			//1 = palindrom 0 != palindrome
+	int bf;					//balance factor
+	treeNode* leftchild; 	//leftmostchild
+	treeNode* rightchild;	//rigthsibling
+};
+
+#endif
