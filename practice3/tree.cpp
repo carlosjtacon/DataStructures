bda85798394819ca7ee3db5b5aa3b4f8576cdad9
mario <mrm994@gmail.com>
2013-12-29 18:37:05 +0100

    now it takes into acount a few more cases that i didn't consider last time still doesn't do the linking procedure

--- practice3/tree.cpp
+++ practice3/tree.cpp
@@ -45,14 +45,10 @@ void insert(string input, treeNode* node) 	//IMPORTANT: right now there's no
 	else if (node->bf = 0)
 	{
 		if (strcmp(input, node->label) < 0)
-		{
 			insert(input, node->leftchild);
-			node->bf = -1;
-		}else if (strcmp(input, node->label) > 0)
-		{
+		else if (strcmp(input, node->label) > 0)
 			insert(input, node->rightchild);
-			node->bf = 1;
-		}else
+		else
 		{
 			//input = node-> label ==> don't know what to do
 		}
@@ -66,8 +62,15 @@ void insert(string input, treeNode* node) 	//IMPORTANT: right now there's no
 		}
 		else if (strcmp(input,node->label) > 0)
 		{
-			swap(node, node->rightchild);
-			insert(input, node->parent->rightchild);
+			if (strcmp(input, node->rightchild->label) > 0)
+			{
+				swap(node, node->rightchild);
+				insert(input, node->parent->rightchild);
+			}
+			else if (strcmp(input, node->rightchild->label) < 0)
+			{
+				//movida de la hostia
+			}			
 			node->bf = 0;
 		}
 		else
@@ -77,15 +80,21 @@ void insert(string input, treeNode* node) 	//IMPORTANT: right now there's no
 	}
 	else if (node->bf = -1)
 	{
-		if (strcmp(input, node->label) > 0)
+		if (strcmp(input, node->label) > 0) //new > parent
 		{
 			insert(input, node->rightchild);
 			node->bf = 0;
 		}
-		else if (strcmp(input, node->label) < 0)
+		else if (strcmp(input, node->label) < 0) //new < parent
 		{
-			swap(node, node->leftchild);
-			insert(input, node->parent->leftchild);
+			if (strcmp(input, node->leftchild->label) < 0) // new < parent.leftchild
+			{
+				swap(node, node->leftchild);
+				insert(input, node->parent->leftchild);
+			}else if (strcmp(input, node->leftchild->label) > 0) //new > parent.leftchild
+			{
+				//movida de la hostia
+			}			
 			node->bf = 0;
 		}
 		else
@@ -99,4 +108,8 @@ void swap(treeNode* parent, treeNode* child)
 {
 	child->parent = parent->parent;
 	parent->parent = child;
+	if (strcmp(child->label, parent->label) < 0)
+		child->rightchild = parent;
+	else if (strcmp(child->label, parent->label) > 0)
+		child->leftchild = parent;
 }
