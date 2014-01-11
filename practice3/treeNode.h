e7e5c8a99877ee9c588f525a1a0e9a0df7aef0e6
mario <mrm994@gmail.com>
2014-01-11 19:15:33 +0100

    included counter of repeated elements in the tree

--- practice3/treeNode.h
+++ practice3/treeNode.h
@@ -5,6 +5,7 @@ struct treeNode
 {
 	string label;
 	bool palindrome;		//1 = palindrom 0 != palindrome
+	int count;				//number of repeated elements
 	int bf;					//balance factor	IF AVL
 	treeNode* leftchild; 	//leftmostchild
 	treeNode* rightchild;	//rigthsibling
